73a35c792046d3c9f4b87af7586bf0e927cb74e2
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-02-16 00:00:01 +0700

    insert to sql before upload to s3

--- app/main.py
+++ app/main.py
@@ -39,20 +39,7 @@ def face_image_input(image: UploadFile = File(...),  # ... = required
                      position_right: int = Form(None),
                      position_bottom: int = Form(None),
                      position_left: int = Form(None)):
-
-    # Upload image to S3
-    s3_resource = boto3.resource('s3',
-                                 endpoint_url=os.getenv('S3_ENDPOINT'),
-                                 aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
-                                 aws_secret_access_key=os.getenv(
-                                     'S3_SECRET_KEY'),
-                                 config=Config(signature_version='s3v4'))
-    bucket_name = os.getenv('S3_BUCKET')
-    bucket = s3_resource.Bucket(bucket_name)
-    bucket.upload_fileobj(image.file, image_name)
-    image_s3_uri = "s3://{0}/{1}".format(bucket_name, image_name)
-    logger.debug("image_s3_uri = {}".format(image_s3_uri))
-
+    
     # Insert data to SQL
     sql_connection = pymysql.connect(host=os.getenv('MYSQL_MASTER_HOST'),
                                      port=int(os.getenv('MYSQL_MASTER_PORT')),
@@ -73,6 +60,21 @@ def face_image_input(image: UploadFile = File(...),  # ... = required
                                     'position_left': position_left})
         sql_connection.commit()  # commit changes
         image_id = cursor.lastrowid  # get last inserted row id
+    sql_connection.close()
+    
+    # Upload image to S3
+    s3_resource = boto3.resource('s3',
+                                 endpoint_url=os.getenv('S3_ENDPOINT'),
+                                 aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
+                                 aws_secret_access_key=os.getenv(
+                                     'S3_SECRET_KEY'),
+                                 config=Config(signature_version='s3v4'))
+    bucket_name = os.getenv('S3_BUCKET')
+    bucket = s3_resource.Bucket(bucket_name)
+    bucket.upload_fileobj(image.file, image_name)
+    image_s3_uri = "s3://{0}/{1}".format(bucket_name, image_name)
+    logger.debug("image_s3_uri = {}".format(image_s3_uri))
+
     # Send data to Kafka
     obj = {'face_image_id': image_id,
            'face_image_path': image_s3_uri,
@@ -85,6 +87,5 @@ def face_image_input(image: UploadFile = File(...),  # ... = required
     kafka_producer.send(os.getenv('KAFKA_TOPIC_FACE_IMAGE'),
                         value=dumps(obj).encode(encoding='UTF-8'))
     
-    sql_connection.close()
     # Return ID to response
     return {'face_image_id': image_id}
