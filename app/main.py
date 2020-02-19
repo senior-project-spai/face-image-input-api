3fbe898d26a7b4d3b4eab698ca2c909d291ba0a0
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-02-19 18:28:16 +0700

    change pool size

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ config = {
 }
 
 pool = mysql.connector.connect(pool_name="mypool",
-                               pool_size=3,
+                               pool_size=2,
                                **config)
 
 
