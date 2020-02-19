152295b574156848ff2b1764e1b1c39d9a9aadfb
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-02-19 23:55:13 +0700

    add time logger

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ def face_image_input(image: UploadFile = File(...),  # ... = required
                      position_right: int = Form(None),
                      position_bottom: int = Form(None),
                      position_left: int = Form(None)):
-
+    req_arrive_time = time1.time()
     # Insert data to SQL
     sql_connection = pymysql.connect(host=os.getenv('MYSQL_MASTER_HOST'),
                                      port=int(os.getenv('MYSQL_MASTER_PORT')),
@@ -55,6 +55,7 @@ def face_image_input(image: UploadFile = File(...),  # ... = required
 
     bucket_name = os.getenv('S3_BUCKET')
     image_s3_uri = "s3://{0}/{1}".format(bucket_name, image_name)
+    sql_start_time = time1.time()
     with sql_connection.cursor() as cursor:
         insert_sql = ("INSERT INTO `FaceImage` (`image_path`, `camera_id`, `branch_id`, `image_time`, `position_top`, `position_right`, `position_bottom`, `position_left`, `time`) "
                       "VALUES (%(image_path)s, %(camera_id)s, %(branch_id)s, %(image_time)s, %(position_top)s, %(position_right)s, %(position_bottom)s, %(position_left)s, %(time)s)")
@@ -70,7 +71,7 @@ def face_image_input(image: UploadFile = File(...),  # ... = required
         sql_connection.commit()  # commit changes
         image_id = cursor.lastrowid  # get last inserted row id
     sql_connection.close()
-
+    s3_start_time = time1.time()
     # Upload image to S3
     s3_resource = boto3.resource('s3',
                                  endpoint_url=os.getenv('S3_ENDPOINT'),
@@ -81,7 +82,7 @@ def face_image_input(image: UploadFile = File(...),  # ... = required
     bucket = s3_resource.Bucket(bucket_name)
     bucket.upload_fileobj(image.file, image_name)
     logger.debug("image_s3_uri = {}".format(image_s3_uri))
-
+    s3_finish_time = time1.time()
     # Send data to Kafka
     obj = {'face_image_id': image_id,
            'face_image_path': image_s3_uri,
@@ -94,6 +95,7 @@ def face_image_input(image: UploadFile = File(...),  # ... = required
         os.getenv('KAFKA_HOST'), os.getenv('KAFKA_PORT'))])
     kafka_producer.send(os.getenv('KAFKA_TOPIC_FACE_IMAGE'),
                         value=dumps(obj).encode(encoding='UTF-8'))
-
+    finish_time = time1.time()
+    logger.info(req_arrive_time,sql_start_time,s3_start_time,s3_finish_time,finish_time)
     # Return ID to response
     return {'face_image_id': image_id}
